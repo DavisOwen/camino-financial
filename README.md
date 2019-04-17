596532f76dc140f0cb42034e763c4eba8e6e5183
Davis Owen <sdowen12@gmail.com>
2019-04-17 03:50:10 +0000

    added unit tests and added to README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # camino financial take home test
+
+# Start docker instance:
+# docker-compose up -d
+# Shut down:
+# docker-compose down
+# Run Unit Tests:
+# docker-compose run web python manage.py test
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from app.models import Loan
+from app.forms import AppForm
 
-# Create your tests here.
+class CreateLoan(TestCase):
+
+    def create(self):
+
+        self.loan = Loan.objects.create(name = "Name", amount = 10000, business = "food_truck", years = 1)
+
+class FormTest(TestCase):
+
+    def test_form_valid(self):
+
+        form = AppForm(data = {"name":"Name", "amount":10000, "business" : "food_truck", "years" : 1})
+        self.assertTrue(form.is_valid())
+
+    def test_form_invalid(self):
+
+        form = AppForm(data = {"name":"", "amount":10, "business":"foo", "years": 1000})
+        self.assertFalse(form.is_valid())
